8fe47ab57b355237366ae7d8c067fdee4c3c13a8
Kevin O'Neill <koneill1994@gmail.com>
2017-09-20 21:00:19 -0400

    Fixed problems with 1st-order markov chain

--- markov.py
+++ markov.py
@@ -4,7 +4,7 @@
 
 # Kevin O'Neill
 
-import random, string, pickle
+import random, string, pickle, sys
 
 
 
@@ -19,7 +19,7 @@ def weighted_choice(choices, total):
    assert False, "Shouldn't get here"
 
 class Markov:
-  # etc
+  
   def __init__(self):
     self.states = {} #name of item, object of item
   
@@ -33,22 +33,37 @@ class Markov:
     for item in data:
       tmp=self.find_state(item)
       if last!=None:
-        last.add_datum(tmp)
+        last.add_datum(item)
       last=tmp
   
   def print_states(self):
+    num_c = 0
+    num_w=0
     for obj in self.states.values():
+      num_w+=1
       print obj.content
       for n in obj.next_state:
-        print "  " + n.content
+        print "  " + n
         print "    " + str(1.0*obj.next_state[n]/obj.next_state_sum)
+        num_c+=1
+    print "DATABASE:"
+    print str(num_w) + " WORDS"
+    print str(num_c) + " CONNECTIONS"
 
+  def generate_text(self):
+    node = self.states[random.choice(self.states.keys())]
+    text=node.content
+    for x in range(0,100):
+      new_node = self.find_state(node.get_next_state())
+      text+=" "+new_node.content
+      node=new_node
+    print text
 
 class MarkovState:
   
   def __init__(self, c):
     self.content = c
-    self.next_state ={}
+    self.next_state = {}
     self.next_state_sum=0
     
   def add_datum(self, datum):
@@ -61,43 +76,51 @@ class MarkovState:
     return weighted_choice(self.next_state, self.next_state_sum)
     
     
+loadOnly=True
+    
+    
 # testing #
+linelimit=None
+
+if(not loadOnly):
+  #'''
+  test = "Let this be the hour when we draw swords together. Fell deeds awake. Now for wrath, now for ruin, and the red dawn".translate(None, string.punctuation).split()
+  
+  m=Markov()
+  m.add_data(test)
+  
+  f = open('lincoln_corpus.txt','r')
+  c=0
+  for line in f:
+    if c%10 == 0:
+      print "reading line " + str(c) + " of lincoln_corpus.txt"
+    m.add_data(line.translate(None, string.punctuation).split())
+    c+=1
+  
+  f.close()
+  
+  #linelimit=1000
+  
+  bible = open('bible_corpus.txt','r')
+  c=0
+  for line in bible:
+    if c%10 == 0:
+      print "reading line " + str(c) + " of bible_corpus.txt"
+    m.add_data(line.translate(None, string.punctuation).split())
+    c+=1
+    if linelimit!= None and c>linelimit:
+      break
+  
+  bible.close()
+  #'''
+  
+  
+  pickle.dump( m, open( "markov_dump.p", "wb" ) )
+
+else:
+  m = pickle.load( open( "markov_dump.p", "rb" ) )
 
 
-#'''
-test = "Let this be the hour when we draw swords together. Fell deeds awake. Now for wrath, now for ruin, and the red dawn".translate(None, string.punctuation).split()
+#m.print_states()
 
-m=Markov()
-m.add_data(test)
-
-f = open('lincoln_corpus.txt','r')
-c=0
-for line in f:
-  if c%10 == 0:
-    print "reading line " + str(c) + " of lincoln_corpus.txt"
-  m.add_data(line.translate(None, string.punctuation).split())
-  c+=1
-
-f.close()
-
-bible = open('bible_corpus.txt','r')
-c=0
-for line in bible:
-  if c%10 == 0:
-    print "reading line " + str(c) + " of bible_corpus.txt"
-  m.add_data(line.translate(None, string.punctuation).split())
-  c+=1
-
-bible.close()
-#'''
-
-
-pickle.dump( m, open( "markov_dump.p", "wb" ) )
-
-
-m = pickle.load( open( "markov_dump.p", "rb" ) )
-
-
-m.print_states()
-
-
+m.generate_text()
